7486006c0b0da548905a711c82611f4c13150690
Peter Hahndorf <hacondev@gmail.com>
2016-10-01 17:56:34 +0200

    Added -cleanup switch to Update-LECertificate.ps1 to remove unused certificates.

--- Update-LECertificate.ps1
+++ Update-LECertificate.ps1
@@ -3,26 +3,30 @@
 param(
     [Parameter(Mandatory=$true,Position=0,ParameterSetName = "all")]
     [switch]$all,
-    [ValidateSet("\LocalMachine\My","\LocalMachine\WebHosting")]
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "all")]
-    [string]$certStore = "\LocalMachine\My",
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "all")]
-    [int]$Days = 20,
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "all")]
-    [int]$MaxNumberOfCerts = 100,
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "all")]
-    [string]$theIssuer = "Let's Encrypt Authority",
+    [Parameter(Mandatory=$true,Position=0,ParameterSetName = "cleanup")]
+    [switch]$cleanup,
     [Parameter(Mandatory=$true,Position=0,ParameterSetName = "schedule")]
     [switch]$schedule,
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "schedule")]
+    [ValidateSet("\LocalMachine\My","\LocalMachine\WebHosting")]
+    [Parameter(Mandatory=$false,ParameterSetName = "all")]
+    [Parameter(Mandatory=$false,ParameterSetName = "cleanup")]
+    [string]$certStore = "\LocalMachine\My",
+    [Parameter(Mandatory=$false,ParameterSetName = "all")]
+    [int]$Days = 20,
+    [Parameter(Mandatory=$false,ParameterSetName = "all")]
+    [int]$MaxNumberOfCerts = 100,
+    [Parameter(Mandatory=$false,ParameterSetName = "cleanup")]
+    [Parameter(Mandatory=$false,ParameterSetName = "all")]
+    [string]$theIssuer = "Let's Encrypt Authority",
+    [Parameter(Mandatory=$false,ParameterSetName = "schedule")]
     [string]$User = $($env:UserDomain + "\" + $env:UserName),
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "schedule")]
+    [Parameter(Mandatory=$false,ParameterSetName = "schedule")]
     [string]$TaskTime = "3:30",
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "schedule")]
+    [Parameter(Mandatory=$false,ParameterSetName = "schedule")]
     [string]$TaskDay = "Sunday",
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "schedule")]
+    [Parameter(Mandatory=$false,ParameterSetName = "schedule")]
     [string]$TaskPath = "\",
-    [Parameter(Mandatory=$false,Position=0,ParameterSetName = "schedule")]
+    [Parameter(Mandatory=$false,ParameterSetName = "schedule")]
     [string]$TaskName = ""
 )
 
@@ -81,6 +85,44 @@ Begin{
         }        
     }
 
+    Function CleanUp-Cert()
+    {
+        Get-ChildItem -Path Cert:$certStore | ForEach-Object {
+            if ($_.Issuer -match "$theIssuer")
+            {        
+                $myCert = $_;
+
+                $mySubject = $myCert.Subject
+                $myThumb = $myCert.Thumbprint
+                [DateTime]$myExpiry = $myCert.notAfter
+
+                Write-Verbose "Looking at $mySubject ($($myThumb)) $myExpiry"
+                # find one with the same subject but later expiry date
+                   Get-ChildItem -Path Cert:$certStore |`
+                    Where-Object {$_.Subject -eq $mySubject -and $_.Thumbprint -ne $myThumb } | ForEach-Object {
+
+                        if ($_.Issuer -match "$theIssuer")
+                        {   
+                            [DateTime]$siblingExpires = $_.notAfter
+                            if ($myExpiry -lt $siblingExpires)
+                            {
+                                Write-Verbose "   Found newer one: $mySubject ($($_.Thumbprint)) $siblingExpires"
+
+                                # check SSL bindings
+                                $inUse = Get-ChildItem IIS:\SslBindings | Where {$_.Thumbprint -eq $myThumb}
+                                if ($inUse -eq $null)
+                                {
+                                    if ($pscmdlet.ShouldProcess("$mySubject ($myThumb)","Remove certificate")){
+                                        $myCert | Remove-Item -Verbose
+                                    }
+                                }
+                            }
+                        }
+                    }                                
+            }
+        }
+    }
+
     Function Schedule-Me()
     {
         if ($TaskPath -notmatch "^\\")
@@ -135,15 +177,9 @@ Begin{
 
 Process 
 {
-    if ($all)
-    {
-        UpdateAll
-    }
-
-    if ($schedule)
-    {
-        Schedule-Me
-    }
+    if ($all)     {UpdateAll}
+    if ($schedule){Schedule-Me}
+    if ($cleanup) {CleanUp-Cert}
 }
 
 <#
@@ -154,12 +190,15 @@ Process
     expire soon and tries to update them.
 .PARAMETER all
     Specifies that all matching certificates should be updated.
+.PARAMETER cleanup
+    Removes all Let's Encrypt certificates that are no longer used in IIS and for each
+    another certificate for the same hostname and a later expired date exists.
 .PARAMETER certStore
     The certificate store in which your certs are stored
 .PARAMETER Days
     Number of days before a certificate expires to be included in the update
 .PARAMETER MaxNumberOfCerts
-    Max number of certs to update. Set to 1 to update just one at a time.
+    Max number of certs to review. Set to 1 to update just one at a time. This include certificate that do not qualify.
 .PARAMETER theIssuer
     String in the issuer field of the certificate.
 .PARAMETER schedule
@@ -189,6 +228,9 @@ Process
 .EXAMPLE
     Update-LECertificate.ps1 -schedule -user joe -TaskName "UpdateCerts" -TaskPath "/adminStuff/" -TaskDay "Monday" -TaskTime "15:20"
     Schedules the script to run every Monday at 3:20 p.m., using user joe with the full name: adminStuff/UpdateCerts
+.EXAMPLE
+    Update-LECertificate.ps1 -cleanup -whatif
+    Shows all certificates that would be removed when not using -whatif
 .NOTES
     Tested on Windows Server 2012 R2
     Author:  Peter Hahndorf
