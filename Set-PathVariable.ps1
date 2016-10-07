9e6067f632a5a164fe1133c40635e3dab2bfcbce
Peter Hahndorf <hacondev@gmail.com>
2016-10-07 14:52:18 +0200

    Set-PathVariable.ps1, first working version

--- Set-PathVariable.ps1
+++ Set-PathVariable.ps1
@@ -1,7 +1,13 @@
-﻿param([string]$NewLocation = "E:\news")
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+    [parameter(Mandatory=$true)]
+    [string]$NewLocation)
 
 Begin
 {
+
+#requires –runasadministrator
+
 $code = @"
 using System;
 using System.Runtime.InteropServices;
@@ -59,17 +65,80 @@ namespace Win32Api
     Function GetOldPath()
     {
         Add-Type -TypeDefinition $code
-        $myReg = new-object Win32Api.RawRegistry
+        $myReg = New-Object Win32Api.RawRegistry
 
+        # Path variable location in the registry
         [string]$keyName = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment";
         [string]$valueName = "Path";
 
-        Return [Win32Api.RawRegistry]::GetUnExpandedValue($keyName,$valueName)    
+        $theValue = [Win32Api.RawRegistry]::GetUnExpandedValue($keyName,$valueName)
+
+        # filled up with NULL characters at the end, remove them
+        Return ($theValue -replace "\x00+$","");
     }
 
 }
 
 Process
 {
-    GetOldPath
+    # Win32API error codes
+    $ERROR_SUCCESS = 0
+    $ERROR_DUP_NAME = 34 
+    $ERROR_INVALID_DATA = 13
+
+    $NewLocation = $NewLocation.Trim();
+
+    If ($NewLocation -eq "" -or $NewLocation -eq $null)
+    {
+        Exit $ERROR_INVALID_DATA
+    }
+   
+    [string]$oldPath = GetOldPath
+    $pattern = ";" + $NewLocation.Replace("\","\\") + ";"
+    $RxInput = ";" + $oldPath + ";"
+
+    Write-Verbose "Old Path: $oldPath"
+
+    # check whether the new location is already in the path
+    if (";$oldPath;" -match $pattern)
+    {
+        Write-Warning "New location is already in the path"
+        Exit $ERROR_DUP_NAME
+    }
+
+    # build the new path, make sure we don't have double semicolons
+    $newPath = $oldPath + ";" + $NewLocation
+    $newPath = $newPath -replace ";;",""
+
+    if ($pscmdlet.ShouldProcess("%Path%", "Add $NewLocation")){
+        # add to the current session
+        $env:path += ";$NewLocation"
+        # save into registry
+        [Environment]::SetEnvironmentVariable("Path", "$newPath", "Machine")
+
+        Write-Output "The operation completed successfully."
+    }
+
+    # http://stackoverflow.com/questions/23813478/set-nested-expandable-environment-variable-with-powershell
+
+    Exit $ERROR_SUCCESS        
 }
+
+<#
+.SYNOPSIS
+    Adds a new item to the machine path environment variable
+.DESCRIPTION
+    Most more popular methods to change the %path% actually break it.
+.PARAMETER NewLocation
+    The new directory location to add at the end of the path. Can be an variable itself.
+.EXAMPLE
+   Set-PathVariable.ps1 -NewLocation "%bin%"
+   Adds the string "%bin%" to the path, when using it it is expanded.
+.NOTES
+    Tested on Windows 10 and Server 2016
+    Author:  Peter Hahndorf
+    Created: October 7th, 2016   
+.LINK
+    https://peter.hahndorf.eu
+    https://github.com/hahndorf/hacops
+#>
